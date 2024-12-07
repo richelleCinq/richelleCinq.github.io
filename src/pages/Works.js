028a48a7e4fbb7cf9586ef8a1e63b33f517d0bc2
Richelle Cinco <rcinco@bu.edu>
2024-12-07 11:21:23 -0700

    new changes to the images and layout for gallery

--- src/pages/Works.js
+++ src/pages/Works.js
@@ -1,5 +1,32 @@
+import React from 'react';
+import '../css/Works.css';
+
+const galleryImages = [
+  'https://richelleCinq.github.io/images/works/BP_triptych.png',
+  'https://richelleCinq.github.io/images/works/Buckingham Pewter-final.png',
+  'https://richelleCinq.github.io/images/works/ikigai.jpg',
+  'https://richelleCinq.github.io/images/works/BP_signage_black.png',
+  'https://richelleCinq.github.io/images/works/tomi_logo.jpg',
+  'https://richelleCinq.github.io/images/works/Tomi_Mockup_front.png',
+  'https://richelleCinq.github.io/images/works/whitewine-momandunuglykids.png',
+  'https://richelleCinq.github.io/images/works/amélie_poster_fin.png',
+  'https://richelleCinq.github.io/images/works/123-tieyourshoe.png',
+  'https://richelleCinq.github.io/images/works/big-spoon-SharpenAI-Motion.png',
+  'https://richelleCinq.github.io/images/works/Big-fork-SharpenAI-Motion.png',
+  'https://richelleCinq.github.io/images/works/Cacao-SharpenAI-Softness.png',
+  'https://richelleCinq.github.io/images/works/coconut-milk-SharpenAI-Softness.png'
+];
+
 const Works = () => {
-    return <h1>Works</h1>;
+    return (
+      <div className="gallery">
+        {galleryImages.map((url, index)=> (
+          <div className="galleryItem" key={index}>
+            <img className="galleryImg" src={url} alt='' />
+          </div>
+        ))}
+      </div>
+    );
   };
   
   export default Works;
